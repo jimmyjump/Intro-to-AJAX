571388e15edf880f2c088ebbfed30e4747ec9647
blago <blago.eres10@gmail.com>
2016-10-21 00:43:43 +0100

    Third commit

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -21,7 +21,33 @@ function loadData() {
     $greeting.text('So, you want to live at ' + address + '?');
 
     var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=1200x400&location=' + address + '';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">')
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+
+    // New York Times AJAX request
+    var NYT_API_KEY = 'eed785fb38e140c2b491faab3e26e83f';
+    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
+                      cityStr +
+                      '&sort=newest&api-key=eed785fb38e140c2b491faab3e26e83f';
+    $.getJSON(nytimesUrl, function(data) {
+
+      $nytHeaderElem.text('New York Times articles about ' + cityStr);
+
+      articles = data.response.docs;
+      for(var i = 0; i < articles.length; i++) {
+        var article = articles[i];
+        $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+      }
+
+    }).error(function(error) {
+      $nytHeaderElem.text('New York Times Articles cannot be loaded');
+    });
+
+
+
+
+
     return false;
 };
 
